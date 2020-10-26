325e09af1adfec21b16e9d03ce1e74c47c634ea8
hanyoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-10-26 17:22:19 +0900

    fontawesome

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTv } from "@fortawesome/free-solid-svg-icons";
 
 class Navbar extends Component {
   render() {
     return (
       <div>
-        <i className="fas fa-tv"></i>
+        <FontAwesomeIcon icon={faTv} className="tvIcon" />
         Hantube
         <form className="searchBar">
           <input
@@ -12,7 +14,9 @@ class Navbar extends Component {
             type="text"
             placeholder="Search.."
           ></input>
-          <button></button>
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
         </form>
       </div>
     );
